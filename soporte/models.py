d7251f31ab62ceff409664464801f1780e4eb3bd
Tobias Maidana <tobiasmaidana611@gmail.com>
2026-08-25 20:29:47 -0300

    Agrega modelo Reparacion y registra la app soporte

--- soporte/models.py
+++ soporte/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Reparacion(models.Model):
+
+    ESTADOS = [
+        ('ESPERA', 'En espera'),
+        ('REPARANDO', 'En reparación'),
+        ('FINALIZADO', 'Finalizado'),
+        ('ENTREGADO', 'Entregado'),
+    ]
+
+    tecnico = models.CharField(max_length=100)
+    diagnostico = models.TextField(blank=True)
+    fecha_entrega = models.DateField(null=True, blank=True)
+    estado = models.CharField(
+        max_length=20,
+        choices=ESTADOS,
+        default='ESPERA'
+    )
+    solucion = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.tecnico} - {self.estado}"
