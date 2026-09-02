6fe1cb276856397168dcd317cabcf7540e2de220
Tobias Maidana <tobiasmaidana611@gmail.com>
2026-09-01 21:30:26 -0300

    Agrega página de inicio y completa CRUD de reparaciones con diseño unificado

--- soporte/urls.py
+++ soporte/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_reparaciones, name='lista_reparaciones'),
-    path('nueva/', views.nueva_reparacion, name='nueva_reparacion'),
+    path('', views.inicio, name='inicio'),
+    path('reparaciones/', views.lista_reparaciones, name='lista_reparaciones'),
+    path('reparaciones/nueva/', views.nueva_reparacion, name='nueva_reparacion'),
+    path('reparaciones/editar/<int:id>/', views.editar_reparacion, name='editar_reparacion'),
+    path('reparaciones/eliminar/<int:id>/', views.eliminar_reparacion, name='eliminar_reparacion'),
 ]
--- soporte/views.py
+++ soporte/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Reparacion
+
+def inicio(request):
+    return render(request, 'soporte/inicio.html') 
 
 def lista_reparaciones(request):
     reparaciones = Reparacion.objects.all().order_by('-id')
@@ -25,3 +28,30 @@ def nueva_reparacion(request):
         return redirect('lista_reparaciones')
 
     return render(request, 'soporte/nueva.html')
+
+def editar_reparacion(request, id):
+    reparacion = get_object_or_404(Reparacion, id=id)
+
+    if request.method == 'POST':
+        reparacion.tecnico = request.POST.get('tecnico')
+        reparacion.diagnostico = request.POST.get('diagnostico')
+        reparacion.estado = request.POST.get('estado')
+        reparacion.fecha_entrega = request.POST.get('fecha_entrega') or None
+        reparacion.solucion = request.POST.get('solucion')
+        reparacion.save()
+        return redirect('lista_reparaciones')
+
+    return render(request, 'soporte/editar.html', {
+        'reparacion': reparacion
+    })
+
+def eliminar_reparacion(request, id):
+    reparacion = get_object_or_404(Reparacion, id=id)
+
+    if request.method == 'POST':
+        reparacion.delete()
+        return redirect('lista_reparaciones')
+
+    return render(request, 'soporte/eliminar.html', {
+        'reparacion': reparacion
+    })
